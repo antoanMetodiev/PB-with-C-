be5751a46f33b07ef333ba2276fdc9e4dc067c55
Antoan Metodiev <Boksnem@abv.bg>
2024-08-23 21:33:11 +0300

    add solution

---
+++ First Steps In Coding/PetShop/PetShop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dogFood; cin >> dogFood;
+    int catFood; cin >> catFood;
+    cout << (dogFood * 2.50) + (catFood * 4.00) << " lv.";
+}
