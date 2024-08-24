a1fda01af4ca3004356c302bbb7e6c06fb7616b4
Antoan Metodiev <Boksnem@abv.bg>
2024-08-24 20:24:51 +0300

    add solution

---
+++ Conditional Statements/Conditional Statements - Lab/PasswordGuess/PasswordGuess.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string password; cin>> password;
+	if (password == "s3cr3t!P@ssw0rd") cout << "Welcome";
+	else cout << "Wrong password!";
+}
+
