1446d590627a457a6211e01a237682d1e3155e79
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-08-24 20:21:29 +0300

    Update DepositCalculator.cpp

--- First Steps In Coding/First Steps In Coding - Exercise/DepositCalculator/DepositCalculator.cpp
+++ First Steps In Coding/First Steps In Coding - Exercise/DepositCalculator/DepositCalculator.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int main()
 {
-	// interest - лихва
-
-	// сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
-
 	double depositSum; cin >> depositSum;
 	int deadline; cin >> deadline;
 	double yearInterestPercent; cin >> yearInterestPercent;
@@ -14,6 +10,5 @@ int main()
 	double perMonth = (yearInterestPercent / 12);
 
 	double sum = depositSum + (deadline * perMonth);
-
 	cout << sum;
 }
