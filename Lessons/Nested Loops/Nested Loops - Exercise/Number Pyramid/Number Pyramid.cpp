a1617b086b47f3f551dbe8bb2d559cd9243dcc0a
Antoan Metodiev <Boksnem@abv.bg>
2024-08-26 19:50:51 +0300

    add solution

---
+++ Lessons/Nested Loops/Nested Loops - Exercise/Number Pyramid/Number Pyramid.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    int current = 1; 
+    bool shouldBreak = false; 
+
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= row; col++) {
+            if (current > n) {
+                shouldBreak = true;
+                break;
+            }
+            cout << current << " "; 
+            current++;
+        }
+        cout << endl; 
+
+        if (shouldBreak) { 
+            break;
+        }
+    }
+
+    return 0;
+}
